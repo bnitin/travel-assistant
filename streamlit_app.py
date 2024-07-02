063c3e2a920b248bfa3594b8dfb36c3ee3c9ddbf
Nitin Bahadur <nitinb@gmail.com>
2024-07-01 19:00:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,6 @@ else:
 
 st.text("I am your travel assistant. Let's help you choose your next travel destination")
 st.text(count)
-st.text(next_question)
 
 # let the user know what we intend to do if they are interacting with this for the first time
 if count == 0:
@@ -104,7 +103,7 @@ if count == 0:
 
 # check if we need to get more input from the user
 if count < 3:
-    #st.text("bbbb: " + next_question)
+    st.text("bbbb: " + next_question)
     next_question = st.session_state['next_question']
     prompt = st.text_input(next_question)
     if prompt:
